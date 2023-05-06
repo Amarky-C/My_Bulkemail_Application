d170c5f502aa875997d0f87a255892f5de8d92c8
Uzoamaka Mbakogu <127051767+Amarky-C@users.noreply.github.com>
2023-05-06 22:21:55 +0100

    Update buddy4bulkemail_app.py

--- buddy4bulkemail_app.py
+++ buddy4bulkemail_app.py
@@ -19,7 +19,7 @@ email_msg['To'] = ','.join(input_address_recipients)
 email_msg['Subject'] = subject
 email_msg.set_content(body)
 
-#create a default connection if its taking time to send bulk message
+
 context = ssl.create_default_context()
 
 
@@ -27,4 +27,4 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as mysmtp:
     mysmtp.login(address_sender,password_sender)
     mysmtp.sendmail(address_sender,input_address_recipients, email_msg.as_string())
     print('Hi Uzo message sent successfully to all cohorts')
-#taking time in sending the bulk mails 
+ 
